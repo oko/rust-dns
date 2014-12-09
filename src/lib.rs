f1a9525e497f57b6b1501ad34b07e93c5c8f2e7e
Jacob Okamoto <okam0013@umn.edu>
2014-12-09 11:00:26 -0500

    Move IANA numbers to `dns::number` and add EDNS0.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-extern crate serialize;
-
-pub use errors::IdentifierError;
-
-pub mod classes;
-pub mod rcodes;
-pub mod opcodes;
-pub mod errors;
-pub mod types;
+pub mod number;
---
+++ src/number/mod.rs
@@ -0,0 +1,9 @@
+pub use self::errors::IdentifierError;
+
+pub mod errors;
+
+pub mod classes;
+pub mod opcodes;
+pub mod rcodes;
+pub mod types;
+pub mod edns0codes;
