2735646d9b1c36f0fb48fbb61d97f73cfa75bccf
Jacob Okamoto <okam0013@umn.edu>
2014-12-14 22:08:32 -0500

    Remove script/ directory (out-of-date)

--- script/gen-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CBD=$(basename $(pwd))
-if [ "$CBD" != "script" ]; then
-	exit 2;
-fi
-python gen-iana-rrtypes.py > ../src/rr/rrtype.rs
-python gen-iana-rcodes.py > ../src/msg/rcode.rs
-python gen-iana-opcodes.py > ../src/msg/opcode.rs
